4e5f170cc8d2d6759066c8b313921da77ae96133
Akshay <akshay.badola.cs@gmail.com>
2020-06-09 03:51:05 +0530

    Added functional programming example. It's terrible IMO, LOL

--- assets/js/programming_titles.js
+++ assets/js/programming_titles.js
@@ -1,4 +1,4 @@
-const titles = ['void *ptr', 'The code is Compiling!', 'The Model is Training!'];
+const titles = ['void *ptr', 'The code is Compiling!', 'The Model is Training!', 'void foo() { return bar; }'];
 const index = Math.floor(Math.random() * titles.length);
 const title = titles[index];
 document.title = title;
