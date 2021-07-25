2a4cbdbe8971a54df40a546276c344935104877f
Akshay <akshay.badola.cs@gmail.com>
2021-07-26 05:20:18 +0530

    Couple of new posts and about changes

--- assets/js/about.js
+++ assets/js/about.js
@@ -1,6 +1,7 @@
 function change_about(){
-  const abouts = ["I'm a cool guy (I think)", 'Wait who am I? Where am I? What is this?!', "Who am I? That's a good question. I've been seeking the answer to that all my life. Do you know who you are?"];
+  const abouts = ['I do stuff, sometimes.', 'Wait who am I? Where am I? What is this?!', "Who am I? That's a good question. I've been seeking the answer to that all my life. Do you know who you are?", "I read therefore I think, I think therefore I am, I am therefore I code, I code therefore I'm bored"];
   const index = Math.floor(Math.random() * abouts.length);
   const about = abouts[index];
-  document.querySelector("body > div.wrapper > div.about.shadow > header > p").textContent = about;
+  const element = (document.querySelector("body > div.wrapper > div.about > header > div.author-description")
+    || document.querySelector("body > div.about.shadow > header > div.author-description")).textContent = about;
 }
