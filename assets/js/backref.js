a42be5f4b94e4e81beb4e4b1b205c7cc50035e9a
Akshay <akshay.badola.cs@gmail.com>
2021-07-28 03:31:36 +0530

    Added post on book on recommendations for sheep

--- assets/js/backref.js
+++ assets/js/backref.js
@@ -1,22 +1,31 @@
 function backrefs (){
-    let root_url = window.location.origin + window.location.pathname;
-    let links = document.getElementsByTagName("a");
-    var refs = [];
-    for (let x of links){
-        if (x.href.replace(root_url, "").startsWith("#ref-")){
-            let ref = x.href.replace(root_url, "").replace("#", "");
-            let backref = ref.replace("ref-", "backref-");
-            if (! refs.includes(ref)){
-                refs.push(ref);
-            } else{
-                let num = refs.filter(x => x === ref).length;
-                backref = backref + "-" + String(num);
-            }
-            x.setAttribute("id", backref);
-            let child = document.createElement("a");
-            child.setAttribute("href", "#" + backref);
-            child.textContent = "\xa0^";
-            document.getElementById(ref).children[0].appendChild(child);
+  const root_url = window.location.origin + window.location.pathname;
+  const links = document.getElementsByTagName("a");
+  const refs = [];
+  for (let x of links){
+    if (x.href.replace(root_url, "").startsWith("#ref-")){
+      const ref = x.href.replace(root_url, "").replace("#", "");
+      let backref = ref.replace("ref-", "backref-");
+      if (! refs.includes(ref)){
+        refs.push(ref);
+      } else{
+        const num = refs.filter(x => x === ref).length;
+        backref = backref + "-" + String(num);
+      }
+      x.setAttribute("id", backref);
+      let child = document.createElement("a");
+      child.setAttribute("href", "#" + backref);
+      child.textContent = "\xa0^";
+      if ( ! document.getElementById(ref)){
+        console.log("Could create backref for", ref);
+      } else{
+        const elem = document.getElementById(ref);
+        if (elem.children[0].className.includes("left-margin")){
+          elem.children[1].appendChild(child);
+        } else{
+            elem.children[0].appendChild(child);
         }
+      }
     }
+  }
 }
